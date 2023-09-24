83a1571bf76a420aefc8bd6e808f30a0b6513b82
Carolina Strecht <carolina.stsampaio@gmail.com>
2023-09-24 11:11:13 +0100

    feat: update post

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -1,6 +1,16 @@
+import { EditPostForm } from "@/components/Forms";
+import { getPostById, updatePost } from "@/lib/dummy-api";
+
 //4
 type PageProps = { params: { id: string } };
 
-export default function EditPost({ params: { id } }: PageProps) {
-  return <>edit your post here {id}</>;
+export default async function EditPost({ params: { id } }: PageProps) {
+  const post = await getPostById(id);
+
+  return (
+    <>
+      <div>update post page caralho</div>
+      <EditPostForm post={post} />
+    </>
+  );
 }
